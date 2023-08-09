108c75741dccff0d6daeea045ab4bfc9eccf7e53
Niklas Arnitz <narnitz@churchtools.de>
2023-08-09 09:20:28 +0200

    fixed webpack config

--- next.config.js
+++ next.config.js
@@ -1,12 +1,16 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  webpack: {
-    test: /\.svg$/,
-    exclude: /node_modules/,
-    use: {
-      loader: "svg-react-loader",
-    },
-  },
+	webpack: (config) => {
+		config.module.rules.push({
+			test: /\.svg$/,
+			exclude: /node_modules/,
+			use: {
+				loader: "svg-react-loader",
+			},
+		});
+
+		return config;
+	}
 };
 
 module.exports = nextConfig;
