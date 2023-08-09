b955e2c2a53f6a18ed6a2aa3cfec58c93a3aec50
Niklas Arnitz <narnitz@churchtools.de>
2023-08-09 09:21:02 +0200

    Fixed Eslint

--- next.config.js
+++ next.config.js
@@ -1,3 +1,6 @@
+/* eslint-disable @typescript-eslint/no-unsafe-return */
+/* eslint-disable @typescript-eslint/no-unsafe-call */
+/* eslint-disable @typescript-eslint/no-unsafe-member-access */
 /** @type {import('next').NextConfig} */
 const nextConfig = {
 	webpack: (config) => {
