72a9b617c7741fe72ae6a37454db24ef18eb3afb
Jeremiah Gage <jgage6@oreillyauto.com>
2023-08-29 10:43:35 -0500

    Resolve Vercel Build Errors 1

--- src/schemas/Continent.ts
+++
@@ -1,6 +0,0 @@
-import { Continent } from "@prisma/client";
-import { z } from "zod";
-
-export const ContinentSchema = z.nativeEnum(Continent);
-
-export type ContinentSchemaType = z.infer<typeof ContinentSchema>;
