3734629c47d9522bb2970b5c64f9476420d9f45b
Jeremiah Gage <jgage6@oreillyauto.com>
2023-08-29 11:23:52 -0500

    Resolve Vercel Build Errors 3

--- src/components/ui/CheckboxList.tsx
+++ src/components/ui/CheckboxList.tsx
@@ -1,35 +1,34 @@
 "use client";
 
 import { Checkbox } from "@/components/ui/checkbox";
-import React from "react";
+import React, { useMemo } from "react";
 
 export type CheckboxProps = {
   label: string;
-  value: string;
-  checked: boolean;
+  value?: string;
+  checked?: boolean;
 };
 export type CheckboxListProps = {
   options: CheckboxProps[];
 };
 
 export function CheckboxList({ options }: CheckboxListProps) {
-  return (
-    <div className="m-2 flex flex-col gap-2">
-      {options.map(({ label, value, checked }) => (
-        <div key={value} className="flex-1 items-center space-x-2">
-          <Checkbox
-            className="rounded-full"
-            id={value}
-            defaultChecked={checked}
-          />
-          <label
-            htmlFor={value}
-            className="text-base font-medium leading-none peer-disabled:cursor-not-allowed peer-disabled:opacity-70"
-          >
-            {label}
-          </label>
-        </div>
-      ))}
-    </div>
-  );
+  const renderedOptions = useMemo(() => {
+    return options.map(({ label, value, checked }) => (
+      <div key={value} className="flex-1 items-center space-x-2">
+        <Checkbox
+          className="rounded-full"
+          id={value}
+          defaultChecked={checked}
+        />
+        <label
+          htmlFor={value}
+          className="text-base font-medium leading-none peer-disabled:cursor-not-allowed peer-disabled:opacity-70"
+        >
+          {label}
+        </label>
+      </div>
+    ));
+  }, [options]);
+  return <div className="m-2 flex flex-col gap-2">{renderedOptions}</div>;
 }
