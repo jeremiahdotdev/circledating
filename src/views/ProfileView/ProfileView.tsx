7f21e516172529c8c41f547a3513823e366a43ea
Jeremiah Gage <jgage6@oreillyauto.com>
2023-09-06 15:32:50 -0500

    Vercel Fix

--- src/views/ProfileView/ProfileView.tsx
+++ src/views/ProfileView/ProfileView.tsx
@@ -1,7 +1,8 @@
 import { Loading } from "@/components/Shared/Loading";
 import { Profile } from "@/components/Profile/Profile";
-import { ReadProfileSchemaType } from "@/schemas/Profile";
+import { ProfileSchemaType, ReadProfileSchemaType } from "@/schemas/Profile";
 import { api } from "@/utils/api";
+import { isUrl } from "@/schemas/Link";
 import { memo } from "react";
 import { routerQueryAttributeToString } from "@/utils/routerQueryAttributeToString";
 import { useRouter } from "next/router";
@@ -23,7 +24,10 @@ export const ProfileView: React.FC<ProfileViewProps> = memo(() => {
     bio: request.data.bio ?? "",
     circles: request.data.circles.map((circle) => circle.Circle),
     interactions: null,
-  };
+    links: request.data.links.map((link) =>
+      isUrl(link.href) ? link : { ...link, href: `https://${link.href}` }
+    ),
+  } as ProfileSchemaType;
 
   return (
     <main className="mx-auto flex min-h-navless max-w-screen-lg flex-col items-center justify-between pt-6">
