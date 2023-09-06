509647fcb7473bfcf2cae4e63c87bd1c1e6c0c78
Jeremiah Gage <jgage6@oreillyauto.com>
2023-09-06 15:33:27 -0500

    Vercel Fix

--- src/views/CircleView/CircleProfileView.tsx
+++ src/views/CircleView/CircleProfileView.tsx
@@ -1,6 +1,8 @@
 import { CircleProfile } from "@/components/Circle/CircleProfile";
+import { CircleSchemaType } from "@/schemas/Circle";
 import { Loading } from "@/components/Shared/Loading";
 import { api } from "@/utils/api";
+import { isUrl } from "@/schemas/Link";
 import { memo } from "react";
 import { routerQueryAttributeToString } from "@/utils/routerQueryAttributeToString";
 import { useRouter } from "next/router";
@@ -20,7 +22,12 @@ export const CircleProfileView: React.FC<CircleProfileViewProps> = memo(() => {
 
   if (!request.data) return <Loading />;
 
-  const circle = request.data;
+  const circle = {
+    ...request.data,
+    links: request.data.links.map((link) =>
+      isUrl(link.href) ? link : { ...link, href: `https://${link.href}` }
+    ),
+  } as CircleSchemaType;
 
   return (
     <main className="mx-auto flex min-h-navless max-w-screen-lg flex-col items-center justify-between pt-6">
