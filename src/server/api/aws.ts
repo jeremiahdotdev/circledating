22c48a3affc3a374eb5c68fb63052ced54c422fc
Jeremiah Gage <jgage6@oreillyauto.com>
2023-10-05 15:34:48 -0500

    ENV changes

--- src/server/api/aws.ts
+++ src/server/api/aws.ts
@@ -1,5 +1,5 @@
 export const configuration = {
-  accessKeyId: process.env.AWS_ACCESS_KEY,
-  secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY,
+  accessKeyId: process.env.AWS_SES_ACCESS_KEY,
+  secretAccessKey: process.env.AWS_SES_SECRET_ACCESS_KEY,
   region: "us-east-2",
 };
