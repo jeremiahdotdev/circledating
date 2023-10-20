b22da04add058bf7453f558a2e82595599d3b3eb
Jeremiah Gage <jgage6@oreillyauto.com>
2023-10-19 22:26:13 -0500

    Add banners; Adjust homepage;

--- src/views/HomeView/HomeView.tsx
+++ src/views/HomeView/HomeView.tsx
@@ -2,18 +2,27 @@
 
 import { About } from "@/components/About/About";
 import { Login } from "@/components/Login/Login";
+import { SlideShow } from "@/components/Shared/SlideShow";
 import React, { memo } from "react";
 
 export type HomeViewProps = Record<never, never>;
 
 export const HomeView: React.FC<HomeViewProps> = memo(() => {
+  const banners = [
+    "bg-banner-0",
+    "bg-banner-1",
+    "bg-banner-2",
+    "bg-banner-3",
+    "bg-banner-4",
+  ];
+
   return (
     <div className="flex min-h-window flex-col items-center justify-between">
-      <div className="flex h-navless w-screen items-end justify-end bg-church-sample bg-cover">
+      <SlideShow images={banners}>
         <div className="flex h-full w-full items-center justify-center md:w-1/2">
           <Login />
         </div>
-      </div>
+      </SlideShow>
       <About />
     </div>
   );
